30b44e2db2a94ba080262be4c9332c99c8f0fb97
hxy112292 <983771687@qq.com>
2019-09-09 23:44:37 -0500

    Add movie page

---
+++ src/views/Movie.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="movie">
+    <el-container>
+      <el-main>
+        <el-row>
+          <el-col :span="6" :offset="0">
+            <el-image src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg"></el-image>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Movie'
+}
+</script>
+
+<style scoped>
+
+</style>
