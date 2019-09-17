ccb5b61684dee091a56ce28654952a3dbb845dbb
housirvip <housirvip@outlook.com>
2019-09-17 00:07:52 -0500

    fix app header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
-    <el-menu
-      class="el-menu-demo"
-      mode="horizontal"
-      :default-active="defaultActive"
-      router>
-      <el-menu-item index="/">Movie Talk</el-menu-item>
-      <el-menu-item index="/discover">Discover</el-menu-item>
-      <el-menu-item index="/auth/login">Login</el-menu-item>
-      <el-menu-item index="/auth/signup">SignUp</el-menu-item>
-      <el-menu-item index="/user-center">{{user.uid>0?user.username:'User Center'}}</el-menu-item>
-    </el-menu>
+    <div class="header">
+      <el-menu
+        class="el-menu-demo"
+        mode="horizontal"
+        :default-active="defaultActive"
+        router>
+        <el-menu-item index="/">Movie Talk</el-menu-item>
+        <el-menu-item index="/discover">Discover</el-menu-item>
+      </el-menu>
+      <el-dropdown class="dropdown">
+        <div class="setting"><span>{{user.username||'User Center'}}</span><i class="el-icon-caret-bottom"></i></div>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item v-if="user.uid"><span @click="refreshMe()">RefreshMe</span></el-dropdown-item>
+<!--          <el-dropdown-item><span @click="onModifyPassword()">修改密码</span></el-dropdown-item>-->
+          <el-dropdown-item v-if="user.uid"><span @click="logout()">LogOut</span></el-dropdown-item>
+          <el-dropdown-item v-if="!user.uid"><span @click="login()">Login</span></el-dropdown-item>
+          <el-dropdown-item v-if="!user.uid"><span @click="signup()">SignUp</span></el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-import { UserService } from './services/api'
+import { AccountService, UserService } from './services/api'
 
 export default {
   name: 'App',
@@ -39,6 +48,17 @@ export default {
       } else {
         this.defaultActive = '/'
       }
+    },
+    logout () {
+      AccountService.logout()
+    },
+    refreshMe () {
+    },
+    login () {
+      this.$router.push({ path: '/auth/login' })
+    },
+    signup () {
+      this.$router.push({ path: '/auth/signup' })
     }
   },
   mounted: function () {
@@ -69,6 +89,21 @@ export default {
     color: #2c3e50;
     &.router-link-exact-active {
       color: #42b983;
+    }
+  }
+}
+.header{
+  text-align: right;
+  .dropdown {
+    position: fixed;
+    right: 30px;
+    top: 30px;
+    .setting {
+      cursor: pointer;
+      .el-icon-setting:first-child {
+        font-size: 120px;
+        vertical-align: middle;
+      }
     }
   }
 }
