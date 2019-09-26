e6caee759539c4c65298e9b9cd1502d290e710ff
housirvip <housirvip@outlook.com>
2019-09-26 03:01:19 -0500

    change local zh-cn to en-us

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import App from './App.vue'
 import router from './router'
 import store from './services/store'
 import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
 
 new Vue({
   router,
